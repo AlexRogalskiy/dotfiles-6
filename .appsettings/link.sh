93ccf065d4d62acfc30e865015a87b87e16b73a0
Robert Boll <robertcboll@gmail.com>
2014-06-06 00:07:43 -0400

    customizations

---
+++ .appsettings/link.sh
@@ -0,0 +1,3 @@
+ln -s sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+
+ln -s iterm2/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist 2> /dev/null
