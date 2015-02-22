8874f80b4e3f989062928e240995372c76a72659
Robert Boll <robertcboll@gmail.com>
2015-02-22 14:17:19 -0500

    bootstrap with git required

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 it's just dotfiles‽
 
 ## bootstrap
-mkdir -p ~/.dotfiles && cd ~/.dotfiles && curl -#L https://github.com/robertcboll/dotfiles/tarball/master | tar -xzv --strip-components 1 -C ~/.dotfiles && ~/.dotfiles/bootstrap.sh
+curl -fsSL https://raw.githubusercontent.com/robertcboll/dotfiles/master/bootstrap.sh > /tmp/bootstrap.sh && chmod +x /tmp/bootstrap.sh && /tmp/bootstrap.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,9 @@
 #! /usr/bin/env bash
 
-# to run this
-# curl -fsSL https://raw.githubusercontent.com/robertcboll/dotfiles/master/bootstrap.sh > /tmp/bootstrap.sh && sh /tmp/bootstrap.sh && rm /tmp/bootstrap.sh
-
-if [ -z $NOGIT ]; then
-  curl -#L https://github.com/robertcboll/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles
-else
-  git clone --recursive https://github.com/robertcboll/dotfiles.git ~/.dotfiles
+if [ ! `which git` ]; then
+  echo 'git is required'
+  exit 1
 fi
 
+git clone --recursive https://github.com/robertcboll/dotfiles.git ~/.dotfiles
 exec ~/.dotfiles/install.sh
