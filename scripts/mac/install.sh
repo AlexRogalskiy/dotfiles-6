a9751cdcb770744577a2d945572f4e4f7ae462ad
rob boll <robertcboll@gmail.com>
2016-01-05 21:37:20 -0500

    Update install.sh

--- scripts/mac/install.sh
+++ scripts/mac/install.sh
@@ -4,7 +4,7 @@ set -e
 echo ">> Installing for OS X."
 "$(dirname "${BASH_SOURCE}")"/settings.sh
 
-command -v brew >/dev/null 2>&1) || {
+command -v brew >/dev/null 2>&1 || {
     echo ">> Installing homebrew."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
