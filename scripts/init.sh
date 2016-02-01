502e4d438da577fbb058276f1b7d7177061d68af
rob boll <robertcboll@gmail.com>
2016-01-31 22:38:33 -0500

    set custom theme for pantheon terminal

--- scripts/init.sh
+++ scripts/init.sh
@@ -33,6 +33,9 @@ sudo pip3 install neovim
 command -v gsettings > /dev/null && uname | grep Linux && {
     gsettings set org.pantheon.terminal.settings tab-bar-behavior 'Hide When Single Tab'
     gsettings set org.pantheon.terminal.settings font 'Inconsolata 18'
+    gsettings set org.pantheon.terminal.settings foreground '#CFCFCF'
+    gsettings set org.pantheon.terminal.settings background '#1B1B1B'
+    gsettings set org.pantheon.terminal.settings palette '#1C1C1C:#8B5F61:#D69F74:#747474:#8B373C:#5F3A3C:#87875F:#878787:#444444:#9E744D:#C79A71:#FED975:#9E4C04:#AF0000:#87AFAF:#FFFFFF'
     gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat-interval 15
 }
 
