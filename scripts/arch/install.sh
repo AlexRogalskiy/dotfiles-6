9c5f343764b1983f201d8c9595c61d2a8dabbea9
rob boll <robertcboll@gmail.com>
2016-03-06 21:50:21 -0500

    updates for arch

--- scripts/arch/install.sh
+++ scripts/arch/install.sh
@@ -9,13 +9,26 @@ grep archlinuxfr /etc/pacman.conf > /dev/null || {
     echo Server = http://repo.archlinux.fr/\$arch >> /etc/pacman.conf
 }
 
-pacman --noconfirm -Syu stow neovim xsel htop hfsprogs jdk8-openjdk git mercurial \
-    ruby go python-pip npm nodejs scala gradle maven docker markdown \
-    vagrant virtualbox shellcheck jq keybase tmux \
-    keybase dropbox yaourt
+pacman --noconfirm -Syu stow neovim xsel xorg-xmodmap htop hfsprogs jdk8-openjdk git mercurial \
+    ruby go cmake python-pip python2 python2-pip npm nodejs scala gradle maven docker markdown \
+    vagrant virtualbox shellcheck jq keybase tmux tree \
+    keybase dropbox pantheon-terminal yaourt
 
-echo "after second pacman??"
-sudo -u roboll yaourt --noconfirm -S google-chrome touchegg
+pacman -Q | grep gnome-terminal && pacman --noconfirm -R gnome-terminal
+
+sudo -u roboll yaourt --noconfirm -S google-chrome touchegg \
+    lightdm-webkit2-greeter lightdm-webkit-google-git \
+    gtk-theme-arc paper-icon-theme-git gnome-shell-extension-dynamic-top-bar \
+    ttf-inconsolata-lgc-for-powerline
+
+sed -i /etc/lightdm/lightdm.conf \
+    -e 's/#greeter-session=.*/greeter-session=lightdm-webkit2-greeter/g' \
+    -e 's/#user-session=.*/user-session=gnome/g'
+
+sed -i /etc/lightdm/lightdm-webkit2-greeter.conf -e 's/antergos/lightdm-webkit-google/g'
+
+cp $(dirname "${BASH_SOURCE}")/roboll.jpg /usr/share/roboll.jpg
+echo "Icon=/usr/share/roboll.jpg" >> /var/lib/AccountsService/users/roboll
 
 echo "options hid_magicmouse scroll-speed=50 scroll-acceleration=1" | \
     sudo tee /etc/modprobe.d/magicmouse.conf > /dev/null
--- scripts/init.sh
+++ scripts/init.sh
@@ -29,14 +29,18 @@ echo ">> Installing from npm."
 sudo npm install -g js-yaml jsonlint recess
 
 echo ">> Installing from pip."
-command -v pip && sudo pip install neovim
-command -v pip2 && sudo pip2 install neovim
-command -v pip3 && sudo pip3 install neovim
+command -v pip > /dev/null && sudo pip install neovim
+command -v pip2 > /dev/null && sudo pip2 install neovim
+command -v pip3 > /dev/null && sudo pip3 install neovim
 
-command -v gsettings > /dev/null && uname | grep Linux && {
+command -v gnome-shell-extension-tool > /dev/null && {
+    gnome-shell-extension-tool -e dynamicTopBar@gnomeshell.feildel.fr || true
+}
+
+command -v gsettings > /dev/null && uname | grep -q Linux && {
     # pantheon terminal
     gsettings set org.pantheon.terminal.settings tab-bar-behavior 'Hide When Single Tab' || true
-    gsettings set org.pantheon.terminal.settings font 'Inconsolata 18' || true
+    gsettings set org.pantheon.terminal.settings font 'Inconsolata LGC for Powerline Medium' || true
     gsettings set org.pantheon.terminal.settings foreground '#878787' || true
     gsettings set org.pantheon.terminal.settings background '#202020' || true
     gsettings set org.pantheon.terminal.settings palette '#151515:#AC4142:#7E8D50:#E5B567:#6C99BB:#9E4E85:#7DD5CF:#D0D0D0:#505050:#AC4142:#7E8D50:#E5B567:#6C99BB:#9E4E85:#7DD5CF:#F5F5F5' || true
@@ -50,6 +54,7 @@ command -v gsettings > /dev/null && uname | grep Linux && {
     gsettings set org.gnome.desktop.wm.preferences audible-bell true || true
 
     gsettings set org.gnome.desktop.interface gtk-theme "Arc" || true
+    gsettings set org.gnome.desktop.interface icon-theme "Paper" || true
     gsettings set org.gnome.desktop.interface clock-format 12h || true
 
     gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15 || true
@@ -60,6 +65,11 @@ command -v gsettings > /dev/null && uname | grep Linux && {
     # gnome terminal
     gsettings set org.gnome.Terminal.Legacy.Settings confirm-close false || true
     gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false || true
+
+    # gnome terminal profile
+    uuid=$(gsettings get org.gnome.Terminal.ProfilesList default | sed s/\'//g)
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uuid/ use-system-font false || true
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$uuid/ font "Inconsolata LGC for Powerline Medium 12" || true
 }
 
 [ -n "$GOPATH" ] && export GOPATH=$HOME/go
