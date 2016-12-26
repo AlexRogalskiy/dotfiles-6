0e42bf574c9657f8987d3622a7e319d882075452
rob boll <robertcboll@gmail.com>
2016-12-26 14:37:20 -0500

    rework bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,61 @@
 #! /usr/bin/env bash
 
-set -e
+set -eo pipefail
 
-command -v git >/dev/null 2>&1 || {
-    case $(uname) in
-        Darwin ) xcode-select --install ;;
-        Linux  )
-            if command -v apt-get > /dev/null; then
-                sudo apt-get install -y git
-            fi
-            ;;
-        *      ) echo "!! Install git & try again." && exit 1 ;;
-    esac
+confirm_var() {
+    name=${1}
+    value=$(printenv ${1})
+    read -p "?? Using $name=$value. Continue? "
 }
 
-echo ">> Getting dotfiles."
-if [ ! -d ~/.dotfiles ]; then
-    git clone --recursive --quiet --depth 1 https://github.com/roboll/dotfiles ~/.dotfiles
-else
-    pushd ~/.dotfiles >/dev/null && git pull && popd >/dev/null
-fi
+log_info() {
+    echo ">> $1"
+}
 
-exec ~/.dotfiles/scripts/install.sh
+log_err() {
+    echo "!! $1" >&2
+    exit 1
+}
+
+require_git() {
+    command -v git >/dev/null 2>&1 || {
+        case $(uname) in
+            Darwin ) xcode-select --install ;;
+            Linux  )
+                if command -v apt-get > /dev/null; then
+                    sudo apt-get install -y git
+                fi
+                ;;
+            *      ) echo "!! git is required" && exit 1 ;;
+        esac
+    }
+}
+
+install_dotfiles() {
+    target=${1}
+    remote=${2}
+    if [ ! -d $target ]; then
+        git clone --recursive --quiet --depth 1 $remote $target
+    else
+        pushd $target >/dev/null
+        current=$(git remote show origin)
+        if [ ! $current ~== $remote ]; then
+            log_err "$target has wrong git remote: $current (expected $remote)"
+        fi
+        git pull
+        popd >/dev/null
+    fi
+}
+
+export INSTALL_DIR=${INSTALL_DIR:=~/.dotfiles}
+confirm_var "INSTALL_DIR"
+
+export DOTFILES=${DOTFILES:=https://github.com/roboll/dotfiles}
+confirm_var "DOTFILES"
+
+require_git
+
+log_info "Getting dotfiles from $DOTFILES."
+install_dotfiles $INSTALL_DIR $DOTFILES
+
+exec $INSTALL_DIR/install.sh
