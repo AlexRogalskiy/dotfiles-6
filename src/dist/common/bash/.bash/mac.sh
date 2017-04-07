be9f46134b10b3b4a585b3bc2a4e9e4acbc04b88
rob boll <robert.boll@datadoghq.com>
2017-04-07 14:59:51 -0400

    fix behavior with ssh colors

--- src/dist/common/bash/.bash/mac.sh
+++ src/dist/common/bash/.bash/mac.sh
@@ -21,13 +21,14 @@ function iterm-reset() {
 function iterm-ssh() {
     if [[ -n "$ITERM_SESSION_ID" ]]; then
         trap "iterm-reset" INT EXIT
-        if [[ "$*" =~ "*prod*" ]]; then
+        if grep -q "prod" <<< $*; then
             iterm-profile ssh-prod
         else
             iterm-profile ssh
         fi
     fi
     ssh $*
+    iterm-reset
 }
 
 alias ssh="iterm-ssh"
