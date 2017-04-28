f8eab20c4a8e63eb73937ab338f96eabc118004f
rob boll <robertcboll@gmail.com>
2017-04-28 01:17:00 -0400

    remove bootstrap stuff

--- bootstrap/README.md
+++
@@ -1,3 +0,0 @@
-# bootstrap
-
-Scripts for bootstrapping hardware.
--- bootstrap/arch/macbook/complete.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-
-pacman --noconfirm -U /install/drivers/*.xz
-rmmod b43 b43legacy ssb bcm43xx brcm80211 brcmfmac brcmsmac bcma wl || true
-modprobe wl || true
-
-rm -r /install
