84747fdc4f76421a50d017bf78d09c5b1ae9f72b
rob boll <robertcboll@gmail.com>
2018-11-29 20:58:52 -0500

    improve completions

--- home/.bash/autoload/70_completions.sh
+++ home/.bash/autoload/70_completions.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
-# ssh scp sftp
-if [ -e "$HOME/.ssh/config" ]; then
-    complete -o "default" -o "nospace" \
-        -W "$(grep "^Host" ~/.ssh/config | \
-        grep -v "[?*]" | cut -d " " -f2 | tr ' ' '\n')" ssh scp sftp
-fi
+# ssh scp
+function _ssh_completion() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(grep '^Host' ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | grep -v '[?*]' | cut -d ' ' -f 2-)
+
+    COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
+    return 0
+}
+
+complete -F _ssh_completion ssh scp
+
+function _notes_completion() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(notes complete)
+
+    COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
+    return 0
+}
 
 # notes
-complete -o "default" -o "nospace" -W "$(notes complete)" notes
+complete -F _notes_completion notes
+#complete -o "default" -o "nospace" -W "$(notes complete)" notes
