09182b8f9c98d8dba300583c0cce1e43e9e0b306
rob boll <robertcboll@gmail.com>
2021-03-18 18:30:36 -0400

    fix

--- scripts/install2.sh
+++ scripts/install2.sh
@@ -17,7 +17,7 @@ command -v brew >/dev/null 2>&1 || {
 }
 
 # install dotfiles repository
-export DOTFILES_DIR="~/.dotfiles"
+export DOTFILES_DIR="$HOME/.dotfiles"
 export DOTFILES_REMOTE="https://github.com/roboll/dotfiles"
 log_info "Installing $DOTFILES_REMOTE into $DOTFILES_DIR."
 
