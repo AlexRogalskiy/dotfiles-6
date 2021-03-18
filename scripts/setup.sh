0694932f031007fa67ae1dc44e40059127ac48d9
rob boll <robertcboll@gmail.com>
2021-03-18 19:02:52 -0400

    2

---
+++ scripts/setup.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+log_info() {
+    echo ">> $1"
+}
+
+log_err() {
+    echo "!! $1" >&2
+    exit 1
+}
+
+confirm_var() {
+    name=${1}
+    value=$(printenv "${1}")
+    message="Using $name=$value."
+
+    read -rp "?? $message Continue? "
+}
+
+# install homebrew if missing
+command -v brew >/dev/null 2>&1 || {
+    log_info "Installing homebrew."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+# install dotfiles repository
+DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES_REMOTE="https://github.com/roboll/dotfiles"
+log_info "Installing $DOTFILES_REMOTE into $DOTFILES_DIR."
+
+if [ ! -d $DOTFILES_DIR ]; then
+    git clone --recursive --quiet --depth 1 "$DOTFILES_REMOTE" "$DOTFILES_DIR"
+else
+    pushd $DOTFILES_DIR >/dev/null
+    current=$(git config --get remote.origin.url)
+    if [ "$current" != "$DOTFILES_REMOTE" ]; then
+        log_err "$DOTFILES_DIR has wrong git DOTFILES_REMOTE: $current (expected $DOTFILES_REMOTE)"
+    fi
+    git pull
+    popd >/dev/null
+fi
+
+exec $DOTFILES_DIR/scripts/_install.sh
